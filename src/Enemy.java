55c87778b539ec40608a9356424cd0f119a7b074
jeffbahns <jeffbahns@gmail.com>
2016-01-10 18:54:51 -0800

    small changes

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,6 +1,10 @@
 /**
  * Created by jeffbahns on 1/10/16.
  */
-public class Enemy {
+public class Enemy extends Person {
     String type;
+
+    Enemy(String name, int HP, int ATK, int DEF, int AGI) {
+        super(name, HP, ATK, DEF, AGI);
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 /**
  * Created by jeffbahns on 1/10/16.
  */
-public class Player {
+public class Player extends Person {
     ArrayList<String> ITEMS = new ArrayList<>();
+
+    Player(String name, int HP, int ATK, int DEF, int AGI) {
+        super(name, HP, ATK, DEF, AGI);
+    }
 }
